c520ecd604ffd3781a5de3533d1635692a0b46bd
srvdevops <devops@srvinfotech.in>
2026-07-25 16:38:49 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    base: 'https://rera.mycad.in/',
+    base: 'https://rera.mycad.in/build/',
     plugins: [
         laravel({
             input: 'resources/js/app.jsx',
